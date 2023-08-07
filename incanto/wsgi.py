f248a7f38451404e4284f57e6ea0d07d5963e3ee
carlozanieri <carlo.zanieri@gmail.com>
2023-08-08 00:07:53 +0200

    Versione 1.2

--- incanto/wsgi.py
+++ incanto/wsgi.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 import os
 import django
 from django.core.wsgi import get_wsgi_application
+django.settings.configure()
 django.setup(set_prefix=False)
 ####os.environ.setdefault("DJANGO_SETTINGS_MODULE", "incanto.settings")
 os.environ["DJANGO_SETTINGS_MODULE"] = "incanto.settings"
